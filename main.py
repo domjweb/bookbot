a66f0f91d653bd506e793f16c1658399bea06b58
domjweb <dominiquejadonwebb@gmail.com>
2024-05-28 23:35:50 -0400

    Counting words and letters formulas defined

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
     print(file_contents)
+
+def word_count(text):
+    words = text.split()
+    return len(words)
+
+def count_letters(text):
+    letter_counts = {}
+    text = text.lower()
+    for letter in text:
+        if letter in letter_counts:
+            letter_counts[letter] += 1
+        else: 
+            letter_counts[letter] = 1
+    return letter_counts
+
+
+
+
+
+
+
+
