22d04e8e12e0ebac406777b37b8aac2940e6eeae
domjweb <dominiquejadonwebb@gmail.com>
2024-05-29 00:14:56 -0400

    Bookbot 1.0

--- main.py
+++ main.py
@@ -16,10 +16,26 @@ def count_letters(text):
             letter_counts[letter] = 1
     return letter_counts
 
+from collections import Counter
+def count_characters(filepath):
+    with open(filepath, 'r') as file:
+        text = file.read().lower()
+    return Counter(filter(str.isalpha, text))
 
+letter_counts = count_characters('books/frankenstein.txt')
+print(letter_counts)
 
+letter_count_list = [{"letter": char, "num": count} for char, count in letter_counts.items()]
+print(letter_count_list)
 
+def sort_on(item):
+    return item["num"]
 
+letter_count_list.sort(reverse=True, key=sort_on)
+print(letter_count_list)
 
-
-
+print("--- Begin report of books/frankenstein.txt ---")
+print(f"{sum(letter_counts.values())} words found in the document")
+for item in letter_count_list:
+    print(f"The '{item['letter']}' character was found {item['num']} times")
+print("--- End report ---")
