7c716f3ed8cf35d92cfe32d132e36197f7431cdd
domjweb <dominiquejadonwebb@gmail.com>
2025-03-25 11:46:01 -0400

    Added all functionalities for main/stats and created dymanic report format

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import wordstring
+from stats import wordstring, characters, listdict, sort
 
 def get_book_text(path_to_file):
 	# Open the file within the 'with' statement
@@ -9,11 +9,31 @@ def get_book_text(path_to_file):
 	return content
 
 
-
-
 def main():
 	booktext = get_book_text("books/frankenstein.txt")
 	# print(booktext)
 	wordcount = wordstring(booktext)
+	charcount = characters(booktext)
+	list_of_dicts = listdict(charcount)
+	sorted_list_of_dicts = sort(list_of_dicts)
+
+	character_counts = ""
+	for pair in sorted_list_of_dicts:
+		if pair['char'].isalpha():
+			character_counts += f"{pair['char']}: {pair['count']}\n"
+
+	
+	print("============ BOOKBOT ============")
+	print("Analyzing book found at books/frankenstein.txt...")
+	print("----------- Word Count ----------")
+	print(f"Found {wordcount} total words")
+	print("--------- Character Count -------")
+	print(character_counts.strip())
+	print("============= END ===============")
+	
+
+
+	
 
 main()
+
--- stats.py
+++ stats.py
@@ -4,5 +4,45 @@ def wordstring(book_text):
 	textsplit = book_text.split()
 	for word in textsplit:
 		count += 1
+	return count
 	
-	print(f"{count} words found in the document")
+	# print(f"{count} words found in the document")
+	
+def characters(book_text):
+	count = {}
+	
+	# Empty dictionary iterates through and sets up new keys and attributes counts 
+	'''
+	
+    Dictionaries are great for solving counting problems like this where
+	there are many variables to keep track of and also keep the counts for.
+	
+    '''
+	for character in book_text:
+		character = character.lower() # Reassigning is necessary when using a method
+		if character not in count:
+			count[character] = 1
+		else:
+			count[character] += 1
+	# print(count)
+	return count
+
+def listdict(dict):
+	list_of_dicts = []
+	
+	# This method creates a new dictionary in the empty list for each character and count from an existing dictionary
+	for char, count in dict.items():
+		list_of_dicts.append({"char": char, "count": count})
+	
+	return list_of_dicts
+
+def sort(list):
+	# This function sorts the new list of dictionaries based on a key value in the .sort method
+	list.sort(reverse=True, key=lambda d: d["count"])
+	return list
+
+
+
+    
+	
+    
